05910a09cc83c32dd49038deaec850abf7f5eed7
shivamp1998 <shivampandey98@gmail.com>
2023-09-15 22:16:49 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import  Component  from './Component';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import Style from 'styled-components'
 const root = ReactDOM.createRoot(document.getElementById('root'));
